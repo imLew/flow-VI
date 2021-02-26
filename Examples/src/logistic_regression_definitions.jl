440c43657c52ae90e0e633510bed2a52246935ac
lew <kol@posteo.de>
2021-02-26 17:59:37 +0100

    put logistic regression data in a struct

--- Examples/src/logistic_regression_definitions.jl
+++ Examples/src/logistic_regression_definitions.jl
@@ -3,32 +3,36 @@ module LogisticRegression
 
 using Distributions
 
-function generate_2class_samples_from_gaussian(;n₀, n₁, μ₀=0, μ₁=1, Σ₀=1, Σ₁=1, n_dim=1)
-    if n_dim == 1
-        generate_2class_samples(n₀, n₁, Normal(μ₁, Σ₁), Normal(μ₀, Σ₀))
-    else
-        generate_2class_samples(n₀, n₁, MvNormal(μ₁, Σ₁), MvNormal(μ₀, Σ₀))
-    end
-end
-
-function generate_2class_samples(n₀, n₁, dist_0, dist_1)
-    return [ones(Int(n₁)) rand(dist_1, n₁)'; zeros((n₀)) rand(dist_0, n₀)']
-end
-
 σ(a) = 1 / (1 + exp(-a))
 
-function logistic_log_likelihood(D, w)
+module Structs
+    struct Data
+        t  # true value / target
+        x  # input data
+        z  # bias basis function
+        function Data(t, x)
+            new(t, x, [ones(size(x)[1]) x])
+        end
+    end
+end  # Structs
+Data = Structs.Data
+y(D::Data, w) = σ.(D.z*w)
+
+function log_likelihood(D, w)
     t = D[:,1]
     x = D[:,2:end]
     z = [ones(size(x)[1]) x]
     sum( σ.(z*w) )
 end
 
-function logistic_grad_logp(D, w)
-    y = D[:,1]
-    x = D[:,2:end]
-    z = [ones(size(x)[1]) x]
-    sum((y .- σ.(z*w)).*z, dims=1)'
+grad_logp(D, w) = sum((D.t .- y(D,w)).*D.z, dims=1)'
+
+function generate_2class_samples_from_gaussian(;n₀, n₁, μ₀=0, μ₁=1, Σ₀=1, Σ₁=1, n_dim=1)
+    generate_2class_samples(n₀, n₁, MvNormal(μ₁, Σ₁), MvNormal(μ₀, Σ₀))
 end
 
-end # module
+function generate_2class_samples(n₀, n₁, dist_0, dist_1)
+    return Data([ones(Int(n₁)); zeros(Int(n₀))], [rand(dist_1, n₁)'; rand(dist_0, n₀)'])
+end
+
+end # logistic regression module
