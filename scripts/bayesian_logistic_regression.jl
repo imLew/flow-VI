906b8b620dfe068eeadb35ffc9bd54da47952090
lew <kol@posteo.de>
2021-02-26 10:23:44 +0100

    WIP make logistic regression run

--- scripts/bayesian_logistic_regression.jl
+++ scripts/bayesian_logistic_regression.jl
@@ -1,69 +1,74 @@
 using DrWatson
 using KernelFunctions
 using BSON
+using Distributions
+using LinearAlgebra
+using Plots
 
 using Utils
+using SVGD
+using Examples
+const LogReg = Examples.LogisticRegression
 
 include("run_funcs.jl")
 
 DIRNAME = "bayesian_logistic_regression"
 
 alg_params = Dict(
-    :kernel => TransformedKernel(SqExponentialKernel(), ScaleTransform(1.)),
-    :kernel_cb => median_trick_cb,
-    :step_size => 0.05,
-    :n_iter => 1000,
-    :n_particles => 100,
+    :update_method => [ :forward_euler ],
+    :kernel => [ TransformedKernel(SqExponentialKernel(), ScaleTransform(1.)) ],
+    :kernel_cb => [ median_trick_cb! ],
+    :step_size => [ 0.001 ],
+    :n_iter => [ 5000 ],
+    :n_particles => [ 20 ],
     )
 
 problem_params = Dict(
-    :n_dim => 2,
-    :n₀ => 100,
-    :n₁ => 100,
-    :μ₀ => [0., 0],
-    :μ₁ => [5, 1],
-    :Σ₀ => [.5 0.1; 0.1 0.2],
-    :Σ₁ => [5 0.1; 0.1 2],
-    :μ_initial => [0., 0, 0],
-    :Σ_initial => [1. 0 0; 0 1 0; 0 0 1.],
-    )
-
-### Experiments - logistic regression on 2D vectors
-ALG_PARAMS = Dict(
-    :step_size => [0.05, 0.01, 0.005 ],
-    :n_iter => [ 50, 100 ],
-    :n_particles => [ 50, 100],
-    :norm_method => "RKHS_norm",
-    :kernel => TransformedKernel(SqExponentialKernel(), ScaleTransform(1.)),
-    :kernel_cb => median_trick_cb
-    )
-
-PROBLEM_PARAMS = Dict(
-    :n_dim => 2,
-    :n₀ => 100,
-    :n₁ => 100,
-    :μ₀ => [0., 0],
-    :μ₁ => [5, 1],
-    :Σ₀ => [.5 0.1; 0.1 0.2],
-    :Σ₁ => [5 0.1; 0.1 2],
-    :μ_initial => [ [0., 0, 0] ],
-    :Σ_initial => [ [9. 0.5 1; 0.5 8 2;1 2 1.],  [1. 0 0; 0 1 0; 0 0 1.]  ],
+    :n_dim => [ 2 ],
+    :n₀ => [ 100 ],
+    :n₁ => [ 100 ],
+    :μ₀ => [ [0., 0] ],
+    :μ₁ => [ [0., 5] ],
+    :Σ₀ => [ [.5 0.1; 0.1 0.2] ],
+    :Σ₁ => [ [.5 0.1; 0.1 .2] ],
+    :μ_initial => [ [1., 1, 1] ],
+    :Σ_initial => [ [1. 0 0; 0 1 0; 0 0 1.] ],
     )
 
 N_RUNS = 1
 
-# n_alg = dict_list_count(ALG_PARAMS)
-# n_prob = dict_list_count(PROBLEM_PARAMS)
-# @info "$(n_alg*n_prob) total experiments"
-# for (i, pp) ∈ enumerate(dict_list(PROBLEM_PARAMS)), 
-#         (j, ap) ∈ enumerate(dict_list(ALG_PARAMS))
-#     @info "Experiment $((i-1)*n_alg + j) of $(n_alg*n_prob)"
-#     @info "Sampling problem: $pp"
-#     @info "Alg parameters: $ap"
-#     @time run_log_regression(
-#             problem_params=pp,
-#             alg_params=ap,
-#             n_runs=N_RUNS,
-#             DIRNAME=DIRNAME
-#             )
-# end
+ap = dict_list(alg_params)[1]
+pp = dict_list(problem_params)[1]
+data = run_log_regression(pp, ap, N_RUNS)
+
+plt = plot_classes(data[:sample_data])
+plot_prediction!(plt,data)
+
+initial_dist = MvNormal(data[:μ₀], data[:Σ₀])
+H₀ = Distributions.entropy(initial_dist)
+EV = ( numerical_expectation( initial_dist, 
+            w -> LogReg.logistic_log_likelihood(data[:sample_data],w) )
+       + expectation_V(initial_dist, initial_dist) 
+       + 0.5 * logdet(2π * data[:Σ_initial])
+) 
+
+est_logZ_rkhs = estimate_logZ.(H₀, EV, KL_integral(data[:svgd_hist][1]))
+
+norm_plot = plot(data[:svgd_hist][1][:ϕ_norm], title = "φ norm", yaxis = :log)
+int_plot = plot( estimate_logZ.(H₀, EV, KL_integral(data[:svgd_hist][1])) ,title = "log Z", label = "",)
+
+###############################################################################
+## compare results
+hist = data[:svgd_hist][1]
+
+norm_plot = plot(get(hist,:ϕ_norm)[2], title="||φ||", yaxis=:log);
+step_plot = plot(get(hist,:step_sizes)[2], title="ϵ", yaxis=:log);
+# step_plot = plot(hist[:step_sizes], title="ϵ", yaxis=:log);
+# cov_diff = norm.(get(hist, :Σ)[2][2:end] - get(hist, :Σ)[2][1:end-1])
+# cov_plot = plot(cov_diff, title="||ΔΣ||", yaxis=:log);
+int_plot = plot(title="log Z");
+plot!(int_plot, estimate_logZ.([H₀], [EV], KL_integral(hist)), label="rkhs",);
+# fit_plot = plot_results(plot(), q, problem_params);
+plot(norm_plot, int_plot, step_plot, layout=@layout [n i; s])
+
+@info "Value comparison" therm_logZ est_logZ_rkhs 
