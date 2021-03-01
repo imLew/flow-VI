7f0babe2a473bd8e20e420d08ad58f20c8beb341
lew <kol@posteo.de>
2021-03-01 21:33:34 +0100

    compare effect of initial covariance

--- scripts/gaussian_to_gaussian.jl
+++ scripts/gaussian_to_gaussian.jl
@@ -38,49 +38,37 @@ using Examples
 
 include("run_funcs.jl")
 
-DIRNAME = "gaussian_to_gaussian/covariance"
-
 problem_params = Dict(
     :μ₀ => [[0., 0]],
     :μₚ => [[0, 0]],
     :Σₚ => [[1. 0; 0 1.]],
-    :Σ₀ => [[10.0^i * I(2) for i in -5:1:5]],
+    :Σ₀ => [10.0^i .* [1. 0; 0 1] for i in -5:1:5],
 )
 
-
-
-plt = plot()
-function plot_cb(;kwargs...)
-    @unpack q, i = kwargs
-    α = get(kwargs, :α, 0)
-    c₁ = get(kwargs, :c₁, 0)
-    c₂ = get(kwargs, :c₂, 0)
-    initial_dist = MvNormal(problem_params[:μ₀][1], problem_params[:Σ₀][1])
-    target_dist = MvNormal(problem_params[:μₚ][1], problem_params[:Σₚ][1])
-    title = α!=0 ? "WAG $α" : "WNES c₁=$c₁ c₂=$c₂"
-    display(plot_2D_results(initial_dist, target_dist, q, title=title))
-end
+# plt = plot()
+# function plot_cb(;kwargs...)
+#     @unpack q, i = kwargs
+#     α = get(kwargs, :α, 0)
+#     c₁ = get(kwargs, :c₁, 0)
+#     c₂ = get(kwargs, :c₂, 0)
+#     initial_dist = MvNormal(problem_params[:μ₀][1], problem_params[:Σ₀][1])
+#     target_dist = MvNormal(problem_params[:μₚ][1], problem_params[:Σₚ][1])
+#     title = α!=0 ? "WAG $α" : "WNES c₁=$c₁ c₂=$c₂"
+#     display(plot_2D_results(initial_dist, target_dist, q, title=title))
+# end
 
 alg_params = Dict(
     :n_iter => [1000],
     :kernel => [TransformedKernel(SqExponentialKernel(), ScaleTransform(1.))],
     :step_size => [0.05],
-    :n_particles => [50],
+    :n_particles => [100, 200],
     :update_method => [:forward_euler, :naive_WAG, :naive_WNES],
     :α => @onlyif(:update_method == :naive_WAG, [3.1] ),
     :c₁ => @onlyif(:update_method == :naive_WNES, [.1, .5,] ),
     :c₂ => @onlyif(:update_method == :naive_WNES, [.3, 1] ),
     :kernel_cb => [median_trick_cb!],
     # :callback => [plot_cb],
-    :n_runs => 10
+    :n_runs => 20,
 )
 
-n_sets = dict_list_count(alg_params)*dict_list_count(problem_params)
-for (i, ap) ∈ enumerate(dict_list(alg_params))
-    for (j, pp) ∈ enumerate(dict_list(problem_params))
-        println("$(((i-1)*dict_list_count(problem_params)) + j) out of $n_sets")
-        name = run_gauss_to_gauss(problem_params=pp, alg_params=ap, DIRNAME=DIRNAME)
-    end
-end
-
-cmdline_run(alg_params, problem_params, run_gauss_to_gauss)
+cmdline_run(alg_params, problem_params, "gaussian_to_gaussian/covariance", run_gauss_to_gauss)
