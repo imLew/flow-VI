e0a441cb7a658e2f6b6b5228cad69af75c27f8e8
lew <kol@posteo.de>
2021-03-10 21:38:59 +0100

    catch and count failed run (for gaussian)

--- scripts/run_funcs.jl
+++ scripts/run_funcs.jl
@@ -18,19 +18,25 @@ function run(::Val{:gauss_to_gauss} ;problem_params, alg_params, DIRNAME="", sav
     initial_dist = MvNormal(problem_params[:μ₀], problem_params[:Σ₀])
     target_dist = MvNormal(problem_params[:μₚ], problem_params[:Σₚ])
 
+    failed_count = 0
     for i in 1:alg_params[:n_runs]
         @info "Run $i/$(alg_params[:n_runs])"
-        q, hist = SVGD.svgd_sample_from_known_distribution( initial_dist,
-                            target_dist; alg_params=alg_params )
+        try 
+            q, hist = SVGD.svgd_sample_from_known_distribution( initial_dist,
+                                target_dist; alg_params=alg_params )
 
-        H₀ = Distributions.entropy(initial_dist)
-        EV = expectation_V( initial_dist, target_dist)
+            H₀ = Distributions.entropy(initial_dist)
+            EV = expectation_V( initial_dist, target_dist)
 
-        est_logZ_rkhs = estimate_logZ(H₀, EV, KL_integral(hist)[end])
+            est_logZ_rkhs = estimate_logZ(H₀, EV, KL_integral(hist)[end])
 
-        push!(svgd_results, q)
-        push!(svgd_hist, hist)
-        push!(estimation_rkhs, est_logZ_rkhs) 
+            push!(svgd_results, q)
+            push!(svgd_hist, hist)
+            push!(estimation_rkhs, est_logZ_rkhs) 
+        catch e
+            failed_count += 1
+            @show e
+        end
     end
 
     true_logZ = logZ(target_dist)
@@ -41,11 +47,12 @@ function run(::Val{:gauss_to_gauss} ;problem_params, alg_params, DIRNAME="", sav
         tagsave(datadir(DIRNAME, file_prefix * ".bson"),
                     merge(alg_params, problem_params, 
                           @dict(true_logZ, estimation_rkhs, svgd_results,
-                               svgd_hist)),
+                                svgd_hist, failed_count)),
                     safe=true, storepatch=true)
     else
         merge(alg_params, problem_params, 
-              @dict(true_logZ, estimation_rkhs, svgd_results, svgd_hist)
+              @dict(true_logZ, estimation_rkhs, svgd_results, svgd_hist, 
+                    failed_count)
              )
     end
 end
@@ -80,7 +87,8 @@ function fit_linear_regression(problem_params, alg_params, D::LinReg.RegressionD
     return initial_dist, q, hist
 end
 
-function run(::Val{:linear_regression}; problem_params, alg_params, DIRNAME="", save=true)
+function run(::Val{:linear_regression}; problem_params, alg_params, 
+             DIRNAME="", save=true)
     svgd_results = []
     svgd_hist = []
     estimation_rkhs = []
@@ -127,51 +135,6 @@ function run(::Val{:linear_regression}; problem_params, alg_params, DIRNAME="", 
             safe=true, storepatch = false)
 end
 
-function fit_logistic_regression(problem_params, alg_params, D) 
-    function logp(w)
-        ( LogReg.log_likelihood(D, w) 
-            + logpdf(MvNormal(problem_params[:μ₀], problem_params[:Σ₀]), w)
-        )
-    end  
-
-    function grad_logp(w) 
-        ( LogReg.grad_log_likelihood(D, w)
-         .- inv(problem_params[:Σ₀]) * (w-problem_params[:μ₀])
-        )
-    end
-    grad_logp!(g, w) = g .= grad_logp(w)
-    @info test
-
-    # use eithe prior as initial distribution of change initial mean to MAP
-    if problem_params[:MAP_start] || problem_params[:Laplace_start]
-        problem_params[:μ_initial] = Optim.maximizer(
-                                Optim.maximize(logp, grad_logp!, 
-                                               problem_params[:μ₀], LBFGS())
-                               )
-        @info "MAP estimate is " problem_params[:μ_initial]
-        # posterior_mean(problem_params[:ϕ], problem_params[:true_β], D, 
-        #                problem_params[:μ₀], problem_params[:Σ₀])
-    end
-    if problem_params[:Laplace_start]
-        y = LogReg.y(D, problem_params[:μ_initial][1])
-        problem_params[:Σ_initial] = ( inv(problem_params[:Σ_initial]) 
-                                      + D.z' * ( y.*(1 .- y) .* D.z )
-                                      )
-        @info "Using Laplace approximation with " problem_params[:Σ_initial]
-    end
-
-    initial_dist = MvNormal(problem_params[:μ_initial], problem_params[:Σ_initial])
-
-    q = rand(initial_dist, alg_params[:n_particles])
-    # grad_logp(w) = vec( - inv(problem_params[:Σ_initial])
-    #                  * ( w-problem_params[:μ_initial] ) 
-    #                  + LogReg.grad_log_likelihood(D, w)
-    #                 )
-
-    q, hist = svgd_fit(q, grad_logp; alg_params...)
-    return initial_dist, q, hist
-end
-
 function run(;problem_params, alg_params, DIRNAME="", save=true)
     problem_params = copy(problem_params)
     alg_params = copy(alg_params)
@@ -185,7 +148,6 @@ function run(;problem_params, alg_params, DIRNAME="", save=true)
     run(Val(problem_params[:problem_type]), problem_params=problem_params, 
         alg_params=alg_params, DIRNAME=DIRNAME, save=save)
 end
-
 
 function run(::Val{:logistic_regression} ;problem_params, alg_params, DIRNAME="", save=true)
     if haskey(problem_params, :sample_data_file)
