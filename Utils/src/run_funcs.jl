7125c24bdb8ae85d926a79e6723727094aecac7a
lew <kol@posteo.de>
2021-03-30 18:09:48 +0200

    fix bug introduced 3 commits ago

--- Utils/src/run_funcs.jl
+++ Utils/src/run_funcs.jl
@@ -129,6 +129,7 @@ function run_svgd(::Val{:linear_regression}; problem_params, alg_params,
     svgd_results = []
     svgd_hist = MVHistory[]
 
+    initial_dist = MvNormal(problem_params[:μ_initial], problem_params[:Σ_initial])
     failed_count = 0
     for i in 1:alg_params[:n_runs]
         try 
