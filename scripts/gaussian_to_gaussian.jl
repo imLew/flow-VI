4e9a4af5d6ae37bc6e155777e918690047ea9d50
lew <kol@posteo.de>
2021-04-26 15:43:29 +0200

    compare WNES v WAG v euler

--- scripts/gaussian_to_gaussian.jl
+++ scripts/gaussian_to_gaussian.jl
@@ -1,35 +1,7 @@
-#!/usr/bin/env julia
-#$ -binding linear:16 # request cpus
-#$ -N gauss_to_gauss
-#$ -q all.q
-#$ -cwd
-#$ -V
-#$ -t 1-16
-### Run SVGD integration of KL divergence on the problem of smapling from
-### a Gaussian
-########
-### command line arguments:
-### make-dicts - create the parameter dicts with DrWatson
-### run "file" - run the algorithm on the parameters given in "file"
-### run-all - run the script on every file specified in "_research/tmp"
-### make-and-run-all - do `make-dicts` followed by `run-all`
-#### Before executing `run-all` of `make-and-run-all` on the cluster the number
-#### of tasks on line 7 ("#$ -t 1-N#Experiments) must be changed
 using DrWatson
-
-if haskey(ENV, "JULIA_ENVIRONMENT")  # on the cluster
-    quickactivate(ENV["JULIA_ENVIRONMENT"], "SVGD")
-else  # local
-    @quickactivate
-end
-
+@quickactivate
 using KernelFunctions
-using Plots
-# using Distributions
 using LinearAlgebra
-# using ValueHistories
-# using ColorSchemes
-# const colors = ColorSchemes.seaborn_colorblind
 
 using SVGD
 using Utils
@@ -44,35 +16,21 @@ PROBLEM_PARAMS = Dict(
     :random_seed => [ 0 ],
 )
 
-# plt = plot()
-# function plot_cb(;kwargs...)
-#     @unpack q, i = kwargs
-#     α = get(kwargs, :α, 0)
-#     c₁ = get(kwargs, :c₁, 0)
-#     c₂ = get(kwargs, :c₂, 0)
-#     initial_dist = MvNormal(problem_params[:μ₀][1], problem_params[:Σ₀][1])
-#     target_dist = MvNormal(problem_params[:μₚ][1], problem_params[:Σₚ][1])
-#     title = α!=0 ? "WAG $α" : "WNES c₁=$c₁ c₂=$c₂"
-#     display(plot_2D_results(initial_dist, target_dist, q, title=title))
-# end
-
 ALG_PARAMS = Dict(
     :dKL_estimator => [ :RKHS_norm ],
-    :n_iter => [1000],
+    :n_iter => [2000],
     :kernel => [TransformedKernel(SqExponentialKernel(), ScaleTransform(1.))],
-    :step_size => [0.5, 0.05, 0.005],
-    :n_particles => [20],
-    :update_method => [:forward_euler, :naive_WAG, :naive_WNES,
-                        :scalar_Adam, :scalar_RMS_prop,],
-    :α => @onlyif(:update_method == :naive_WAG, [3, 4, 7] ),
-    :c₁ => @onlyif(:update_method == :naive_WNES, [.1, 1, 5] ),
-    :c₂ => @onlyif(:update_method == :naive_WNES, [.1, 1, 5] ),
+    :step_size => [0.05, 0.005],
+    :n_particles => [100],
+    :update_method => [:forward_euler, :naive_WAG, :naive_WNES],
+    :α => @onlyif(:update_method == :naive_WAG, [3.1, 4, 7] ),
+    :c₁ => @onlyif(:update_method == :naive_WNES, [.1, 1, .5] ),
+    :c₂ => @onlyif(:update_method == :naive_WNES, [.1, 1, .5] ),
     :γ => @onlyif(:update_method == :scalar_RMS_prop, [.9, .8] ),
     :β₁ => @onlyif(:update_method == :scalar_Adam, [.9] ),
     :β₂ => @onlyif(:update_method == :scalar_Adam, [.999] ),
     :kernel_cb => [median_trick_cb!],
-    # :callback => [plot_cb],
     :n_runs => 10,
 )
 
-run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/method_compare")
+run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/WNESvWAGvEuler")
