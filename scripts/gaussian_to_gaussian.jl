120e20224b47bec5e15d2e439bcdcc49afd74fa9
lew <kol@posteo.de>
2021-05-03 11:41:31 +0200

    try annealing schedules

--- scripts/gaussian_to_gaussian.jl
+++ scripts/gaussian_to_gaussian.jl
@@ -12,7 +12,7 @@ PROBLEM_PARAMS = Dict(
     :μ₀ => [[0., 0]],
     :μₚ => [[0, 0]],
     :Σₚ => [[1. 0; 0 1.]],
-    :Σ₀ => [ 0.001*I(2), 0.01*I(2), 0.1*I(2), 10.0*I(2), 100.0*I(2), 1000.0*I(2) ],
+    :Σ₀ => [ 0.1*I(2), 10.0*I(2), ],
     :random_seed => [ 0 ],
 )
 
@@ -22,15 +22,14 @@ ALG_PARAMS = Dict(
     :kernel => [TransformedKernel(SqExponentialKernel(), ScaleTransform(1.))],
     :step_size => [ 0.05, 0.005 ],
     :n_particles => [100],
-    :update_method => [:forward_euler, :naive_WAG, :naive_WNES],
-    :α => @onlyif(:update_method == :naive_WAG, [3.1] ),
-    :c₁ => @onlyif(:update_method == :naive_WNES, [.1, .5, @onlyif(:c₂==1.5, [1])] ),
-    :c₂ => @onlyif(:update_method == :naive_WNES, [1, 1.5] ),
-    :γ => @onlyif(:update_method == :scalar_RMS_prop, [.9, .8] ),
-    :β₁ => @onlyif(:update_method == :scalar_Adam, [.9] ),
-    :β₂ => @onlyif(:update_method == :scalar_Adam, [.999] ),
+    :update_method => [:forward_euler],
     :kernel_cb => [median_trick_cb!],
+    :annealing_schedule => [ hyperbolic_annealing, linear_annealing, cyclic_annealing],
+    :annealing_params => Dict( :duration => 0.8,
+                               :p => 10,
+                               :C => 5
+                              ),
     :n_runs => 10,
 )
 
-run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/covariance")
+run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/annealing")
