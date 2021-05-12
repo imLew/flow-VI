7f9fee6238375aeac477e50e015603da268d39fb
lew <kol@posteo.de>
2021-05-12 12:02:21 +0200

    MAPvLaplacevNormal experiments

--- scripts/bayesian_logistic_regression.jl
+++ scripts/bayesian_logistic_regression.jl
@@ -2,14 +2,6 @@ using DrWatson
 @quickactivate
 using KernelFunctions
 using LinearAlgebra
-using AdvancedHMC
-
-using LoggingExtras
-using Logging
-function not_AdvancedHMC_message_filter(log)
-    log._module != AdvancedHMC
-end
-global_logger(EarlyFilteredLogger(not_AdvancedHMC_message_filter, global_logger()))
 
 using Utils
 using SVGD
@@ -19,12 +11,9 @@ DIRNAME = "bayesian_logistic_regression/MAPvLaplacevNormal"
 
 ALG_PARAMS = Dict(
     :update_method => [ :forward_euler ],
-    :α => @onlyif(:update_method == :naive_WAG, [3, 4, 7, 10] ),
-    :c₁ => @onlyif(:update_method == :naive_WNES, [.1, 1, 5] ),
-    :c₂ => @onlyif(:update_method == :naive_WNES, [.1, 1, 5] ),
     :kernel => [ TransformedKernel(SqExponentialKernel(), ScaleTransform(1.)) ],
     :kernel_cb => [ median_trick_cb! ],
-    :step_size => [ 0.001 ],
+    :step_size => [ 0.01, 0.001 ],
     :n_iter => [ 1000 ],
     :n_particles => [ 50 ],
     :n_runs => [ 10 ],
@@ -42,6 +31,8 @@ PROBLEM_PARAMS = Dict(
     :μ₁ => [ [4., 3] ],
     :Σ₀ => [ [0.5 0.1; 0.1 0.2] ],
     :Σ₁ => [ [.5 0.1; 0.1 .2] ],
+    :μ_prior => [ zeros(3) ],
+    :Σ_prior => [ I(3) ],
     :μ_initial => [ [1., 1, 1] ],
     :Σ_initial => [ I(3) ],
     :therm_params => [Dict(
@@ -49,9 +40,6 @@ PROBLEM_PARAMS = Dict(
                           :nSteps => 30
                          )],
     :random_seed => [ 0 ],
-    # :sample_data_file => [datadir("classification_samples",
-    # "2dim_50:[0.0, 0.0]:[0.5 0.1; 0.1 0.2]_50:[0.0, 0.0]:[0.5 0.1; 0.1 0.2].bson")
-    #                      ],
 )
 
 run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, DIRNAME)
