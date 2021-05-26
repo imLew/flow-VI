2f0c5f19a8190f8117db9eff658e2607f4f9e585
lew <kol@posteo.de>
2021-05-26 08:50:04 +0200

    initial grid search on svgd for parameters

--- scripts/gaussian_to_gaussian.jl
+++ scripts/gaussian_to_gaussian.jl
@@ -3,9 +3,9 @@ using DrWatson
 using KernelFunctions
 using LinearAlgebra
 
-using SVGD
+# using SVGD
 using Utils
-using Examples
+# using Examples
 
 PROBLEM_PARAMS = Dict(
     :problem_type => [ :gauss_to_gauss ],
@@ -21,14 +21,10 @@ ALG_PARAMS = Dict(
     :n_iter => [1000, 2000],
     :kernel => [TransformedKernel(SqExponentialKernel(), ScaleTransform(1.))],
     :step_size => [ 0.05, 0.001, 0.005 ],
-    :n_particles => [50, 100],
-    :update_method => [:forward_euler, :scalar_Adam, :scalar_adagrad,
-                       @onlyif(:n_iter == 1000, [:scalar_RMS_prop])],
-    :β₁ => @onlyif(:update_method==:scalar_Adam ,[ 0.9 ]),
-    :β₂ => @onlyif(:update_method==:scalar_Adam ,[ 0.999 ]),
-    :γ => @onlyif(:update_method==:scalar_RMS_prop ,[ 0.8, 0.9 ]),
+    :n_particles => [50, 100, 200],
+    :update_method => [ :forward_euler ],
     :kernel_cb => [median_trick_cb!],
     :n_runs => 10,
 )
 
-run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/gd_variants")
+run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/initial_grid")
