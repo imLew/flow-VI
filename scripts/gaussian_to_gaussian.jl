fb464d2006ae227854aeb1bc7345d121a1e5a68c
lew <kol@posteo.de>
2021-05-29 18:10:56 +0200

    comparison of log scale variation of initial cov

--- scripts/gaussian_to_gaussian.jl
+++ scripts/gaussian_to_gaussian.jl
@@ -9,19 +9,18 @@ PROBLEM_PARAMS = Dict(
     :μ₀ => [[0., 0]],
     :μₚ => [[0, 0]],
     :Σₚ => [[1. 0; 0 1.]],
-    :Σ₀ => [ 0.1*I(2), 10.0*I(2), ],
-    # :Σ₀ => [ 0.001*I(2), 0.01*I(2), 0.1*I(2), 10.0*I(2), 100.0*I(2), 1000.0*I(2), ],
+    :Σ₀ => [ 0.001*I(2), 0.01*I(2), 0.1*I(2), 10.0*I(2), 100.0*I(2), 1000.0*I(2), ],
     :random_seed => [ 0 ],
 )
 
 ALG_PARAMS = Dict(
     :dKL_estimator => [ :RKHS_norm ],
-    :n_iter => [ 3000, 4000 ],
+    :n_iter => [ 1000 ],
     :step_size => [ 0.05 ],
-    :n_particles => [ 100, 200 ],
+    :n_particles => [ 50 ],
     :update_method => [ :forward_euler ],
     :kernel_cb => [ median_trick_cb! ],
     :n_runs => 10,
 )
 
-run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/initial_grid_follow_up")
+run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/cov_comparison")
