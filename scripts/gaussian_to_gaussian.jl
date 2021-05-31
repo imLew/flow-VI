c88b8eaa2dae6970ebccf52675785532542437cf
lew <kol@posteo.de>
2021-05-31 09:53:18 +0200

    log cov_comparison with more factors

--- scripts/gaussian_to_gaussian.jl
+++ scripts/gaussian_to_gaussian.jl
@@ -9,18 +9,19 @@ PROBLEM_PARAMS = Dict(
     :μ₀ => [[0., 0]],
     :μₚ => [[0, 0]],
     :Σₚ => [[1. 0; 0 1.]],
-    :Σ₀ => [ 0.001*I(2), 0.01*I(2), 0.1*I(2), 10.0*I(2), 100.0*I(2), 1000.0*I(2), ],
+    :Σ₀ => [ 10.0^i*I(2) for i in -3:0.2:3 ],
     :random_seed => [ 0 ],
 )
 
 ALG_PARAMS = Dict(
     :dKL_estimator => [ :RKHS_norm ],
-    :n_iter => [ 1000 ],
-    :step_size => [ 0.05 ],
+    :n_iter => [ 5000 ],
+    :step_size => [ 0.01 ],
     :n_particles => [ 50 ],
     :update_method => [ :forward_euler ],
     :kernel_cb => [ median_trick_cb! ],
     :n_runs => 10,
 )
 
-run_single_instance(PROBLEM_PARAMS, ALG_PARAMS, "gaussian_to_gaussian/cov_comparison")
+run_single_instance(PROBLEM_PARAMS, ALG_PARAMS,
+                    "gaussian_to_gaussian/cov_comparison",)
