cba03069267500e29268cb67c58d94d0c8da1585
lew <kol@posteo.de>
2021-07-13 14:42:17 +0200

    rerun for number of particles on gaussian for better plot

---
+++ scripts/gaussian_number_particles.jl
@@ -0,0 +1,28 @@
+using DrWatson
+@quickactivate
+using LinearAlgebra
+
+using Utils
+
+
+PROBLEM_PARAMS = Dict(
+    :problem_type => :gauss_to_gauss,
+    :μ₀ => [[0., 0]],
+    :μₚ => [[0, 0]],
+    :Σₚ => [[1. 0; 0 1.]],
+    :Σ₀ => [ 0.1*I(2), 10.0*I(2), ],
+    :random_seed => 0,
+)
+
+ALG_PARAMS = Dict(
+    :dKL_estimator => :RKHS_norm,
+    :n_iter => 4000,
+    :step_size => [ 0.01, 0.001, 0.005 ],
+    :n_particles => [50, 100, 200],
+    :update_method => :forward_euler,
+    :kernel_cb => median_trick_cb!,
+    :n_runs => 10,
+)
+
+run_single_instance(PROBLEM_PARAMS, ALG_PARAMS,
+                    "gaussian_to_gaussian/number_particles",)
